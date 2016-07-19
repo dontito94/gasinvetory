244ddb64473b1817e21c4ddb42cc974efd86839a
dontito94 <francistito07@gmail.com>
2016-07-19 17:14:30 +0300

    addition of auth

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -28,7 +28,9 @@ class AuthController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $loginPath = '/login';
+     protected $redirectTo = '/home';
+      protected $redirectAfterLogout = '/login';
 
     /**
      * Create a new authentication controller instance.
--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -21,8 +21,7 @@ class Authenticate
             if ($request->ajax() || $request->wantsJson()) {
                 return response('Unauthorized.', 401);
             } else {
-                return redirect()->guest('login');
-            }
+             return redirect()->guest('/login');            }
         }
 
         return $next($request);
